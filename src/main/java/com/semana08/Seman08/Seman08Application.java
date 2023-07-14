94637383505c24d190411d2ff1bc9b4973492b08
MiguelAngel2022Sistemas <113484673+MiguelAngel2022Sistemas@users.noreply.github.com>
2023-07-14 01:05:55 +0000

    GUARDAR

---
+++ src/main/java/com/semana08/Seman08/Seman08Application.java
@@ -0,0 +1,13 @@
+package com.semana08.Seman08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seman08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seman08Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/semana08/Seman08/Seman08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana08.Seman08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seman08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
